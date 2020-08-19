2b39bb688f349519110268ef48d9ff90ee96e460
Rufin Hounkpe <rhounkpe@gmail.com>
2020-08-19 15:53:59 +0200

    Initial Commit

---
+++ MemeMeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMeApp
+//
+//  Created by Rufin Hounkpe on 19/08/2020.
+//  Copyright © 2020 Digit Com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
